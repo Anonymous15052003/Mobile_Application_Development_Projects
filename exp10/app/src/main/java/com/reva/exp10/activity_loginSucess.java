cc5c64ba2e2718d14a6626310ee796937375e835
Anonymous15052003 <sharonpro24x7@gmail.com>
2024-05-05 20:12:59 +0530

    .

---
+++ exp10/app/src/main/java/com/reva/exp10/activity_loginSucess.java
@@ -0,0 +1,12 @@
+package com.reva.exp10;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_loginSucess extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sucess);
+    }
+}
